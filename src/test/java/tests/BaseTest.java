ff4deca4e887aae279066ea606c55ddd5a745837
OlgaKaratkevich <volhakaratkevich@gmail.com>
2025-02-07 03:30:41 +0300

    add BaseTest

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://app.qase.io";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 5000;
+        Configuration.pollingInterval = 200;
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        closeWebDriver();
+    }
+}

be21f1a45db0f0fae54d728660196fb9fe072fd2
OlgaKaratkevich <volhakaratkevich@gmail.com>
2025-02-28 17:25:03 +0300

    add negative project tests

--- src/test/java/pages/ProjectPage.java
+++ src/test/java/pages/ProjectPage.java
@@ -1,10 +1,13 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.conditions.Visible;
 import io.qameta.allure.Step;
 import tests.api.pojos.request.project.CreateProjectRequest;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 import static pages.elements.Button.clickButton;
@@ -14,7 +17,7 @@ public class ProjectPage extends BasePage{
     private final SelenideElement PROJECT_PAGE_TITLE = $x("//div[@id='application-content']//h1");
     private final SelenideElement PROJECT_NAME_INPUT = $("#project-name");
     private final SelenideElement PROJECT_CODE_INPUT = $("#project-code");
-    private final SelenideElement PROJ_ERROR = $x("//div[text()='The code must be at least 2 characters.']");
+    private final SelenideElement PROJECT_CODE_ERROR = $x("//div[text()='%s']");
     private final SelenideElement DELETE_BUTTON = $x("//div[@data-testid = 'remove']");
     private final SelenideElement DELETE_PROJECT_BUTTON = $x("//span[text() = 'Delete project']");
 
@@ -23,12 +26,35 @@ public class ProjectPage extends BasePage{
         clickButton("Create new project");
     }
 
+    @Step("Create Project")
+    public void createProject()
+    {
+        clickButton("Create project");
+    }
+
     @Step("Input project data")
     public void inputProjectData(CreateProjectRequest project){
         PROJECT_NAME_INPUT.sendKeys(project.getTitle());
         PROJECT_CODE_INPUT.clear();
         PROJECT_CODE_INPUT.sendKeys(project.getCode());
         clickButton("Create project");
+    }
+
+    @Step("Input project title")
+    public void inputProjectTitle(String title){
+        PROJECT_NAME_INPUT.sendKeys(title);
+    }
+
+
+    @Step("Input project code")
+    public void inputProjectCode(String code){
+        PROJECT_CODE_INPUT.clear();
+        PROJECT_CODE_INPUT.sendKeys(code);
+    }
+
+    @Step("Project code error should be seen")
+    public void projectErrorIsVisible(String errorText) {
+        PROJECT_CODE_ERROR.shouldBe(visible);
     }
 
     @Step("Check if Project page is open")
--- src/test/java/tests/ui/ProjectTest.java
+++ src/test/java/tests/ui/ProjectTest.java
@@ -3,13 +3,18 @@ package tests.ui;
 import generators.ProjectGenerator;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import tests.BaseTest;
 import tests.api.pojos.request.project.CreateProjectRequest;
+
+import java.util.stream.Stream;
 
 import static generators.ProjectGenerator.createProjectApi;
 
 @Tag("UI")
-public class ProjectTest extends BaseTest{
+public class ProjectTest extends BaseTest {
 
     @Test
     public void createProjectTest() {
@@ -24,7 +29,24 @@ public class ProjectTest extends BaseTest{
         ProjectGenerator.deleteProjectApi(project.getCode());
     }
 
+    static Stream<Arguments> projectNegativeTests() {
+        return Stream.of(
+                Arguments.of("Title", "A", "The code must be at least 2 characters."),
+                Arguments.of("Title", "asdfgqwrrtgsdsdg", "The code may not be greater than 10 characters." )
+        );
+    }
 
-
-
+    @MethodSource("projectNegativeTests")
+    @ParameterizedTest(name = "Auth negative login  tests")
+    void authNegativeTests(String title, String code, String errorText) {
+        loginPage.openLoginPage();
+        loginPage.inputLogin("roxeve9247@fenxz.com");
+        loginPage.inputPass("AsdQwerty123!");
+        loginPage.clickSignInButton();
+        projectPage.clickCreateNewProjectButton();
+        projectPage.inputProjectTitle(title);
+        projectPage.inputProjectCode(code);
+        projectPage.createProject();
+        projectPage.projectErrorIsVisible(errorText);
+    }
 }
